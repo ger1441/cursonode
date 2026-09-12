c39e63d085f4ae2650400544410159dc13bc5958
yerman <gerql1441@gmail.com>
2026-09-12 16:00:33 -0600

    Información del sistema con ES Modules

---
+++ 2.fs.js
@@ -0,0 +1,4 @@
+// fs => FileSystem (Sistema de archivos)
+const fs = require('node:fs'); // a partir de Node 16, se recomienda poner node: antes del módulo nativo
+
+// fs.readFileSync() => lee un archivo de manera síncrona
