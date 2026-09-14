9941ef266563e389981239de6f7d576bf4731e23
yerman <gerql1441@gmail.com>
2026-09-14 16:13:29 -0600

    Instalando dependencias con npm

--- 8.ls-advance.js
+++ 8.ls-advance.js
@@ -1,5 +1,6 @@
 const fs = require('node:fs/promises');
 const path = require('node:path');
+const picocolors = require('picocolors');
 
 const folder = process.argv[2] ?? '.';
 console.log('Leyendo el contenido de la carpeta:', folder);
@@ -9,7 +10,7 @@ async function ls (folder) {
     try {
         files = await fs.readdir(folder);
     } catch (err) {
-        console.error("Error al leer el directorio: ", folder);
+        console.error(picocolors.red(`Error al leer el directorio: ${folder}`));
         process.exit(1);
     }
 
@@ -28,7 +29,7 @@ async function ls (folder) {
         const fileSize = stats.size;
         const fileModified = stats.mtime.toLocaleString();
 
-        return `${symbol} ${file.padEnd(20)} ${fileSize.toString().padStart(10)} bytes ${fileModified}`;
+        return `${symbol} ${picocolors.blue(file.padEnd(20))} ${fileSize.toString().padStart(10)} bytes ${fileModified}`;
     });
 
     const filesInfo = await Promise.all(filePromises);
