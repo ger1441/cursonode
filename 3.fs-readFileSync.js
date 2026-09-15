ab219312a52c6bad14f0412d212e91d24c7cc73a
yerman <gerql1441@gmail.com>
2026-09-15 17:30:15 -0600

    Normaliza saltos de linea

--- 3.fs-readFileSync.js
+++
@@ -1,9 +0,0 @@
-const fs = require('node:fs') // a partir de Node 16, se recomienda poner node: antes del módulo nativo
-
-console.log('Leyendo primer archivo....')
-const text = fs.readFileSync('./archivo.txt', 'utf-8') // devuelve el contenido del archivo
-console.log('Primer texto:' + text)
-
-console.log('Leyendo el segundo archivo....')
-const secondText = fs.readFileSync('./archivo2.txt', 'utf-8') // devuelve el contenido del archivo
-console.log('Segundo texto:' + secondText)
--- cjs/sum.js
+++
@@ -1,8 +0,0 @@
-function sum(a, b) {
-  return a + b;
-}
-
-// CommonJS module export
-module.exports = {
-    sum
-}
