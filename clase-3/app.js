9d4772f0b1cef916107f161d869564a15ead9210
yerman <gerql1441@gmail.com>
2026-09-16 15:10:35 -0600

    GET, POST, PATCH, validaciones con esquemas

---
+++ clase-3/app.js
@@ -0,0 +1,80 @@
+const express = require('express') // require -> commonJS
+const crypto = require('node:crypto')
+const movies = require('./movies.json')
+const { validateMovie, validatePartialMovie } = require('./schemas/movies')
+
+const app = express()
+app.disable('x-powered-by')
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.json({ message: 'hola mundo' })
+})
+
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+    console.log('filtrado')
+    return res.json(filteredMovies)
+  }
+  res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+
+  res.status(404).json({ message: 'No se encontró el recurso' })
+})
+
+app.post('/movies', (req, res) => {
+  console.log('llego')
+  const result = validateMovie(req.body)
+  if (result.error) {
+    // 422 tambien se puede usar
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const newMovie = {
+    id: crypto.randomUUID(), // uuid v4
+    ...result.data
+  }
+  // Esto NO sería REST porque estmoas guardando
+  // el estado de la aplicación en memoria
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie)
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+  if (result.error) {
+    // 422 tambien se puede usar
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+  return res.json(updateMovie)
+})
+
+const PORT = process.env.PORT ?? 3000
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
