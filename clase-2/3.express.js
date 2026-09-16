be886f3a50ffe1ab2e395506b30a62146227b319
yerman <gerql1441@gmail.com>
2026-09-16 09:21:12 -0600

    express

--- clase-2/3.express.js
+++ clase-2/3.express.js
@@ -1,10 +1,39 @@
 const express = require('express')
 const app = express()
+app.disable('x-powered-by')
 
 const PORT = process.env.PORT ?? 3000
 
 app.get('/', (req, res) => {
-  res.status(200).send('<h1>Mi pagina</h1>')
+  res.status(200).send('<h1>Mi página</h1>')
+})
+
+app.get('/pokemon/ditto', (req, res) => {
+  const dittoJSON = require('./pokemon/dito.json')
+  res.send(JSON.stringify(dittoJSON))
+})
+
+app.get('/pokemon/psyduck', (req, res) => {
+  const psyduckJSON = require('./pokemon/psyduck.json')
+  res.json(psyduckJSON)
+})
+
+app.post('/pokemon', (req, res) => {
+  let body = ''
+  // escuchar el evento data
+  req.on('data', chunk => {
+    body += chunk.toString()
+  })
+
+  req.on('end', () => {
+    const data = JSON.parse(body)
+    data.timestamp = Date.now()
+    res.status(200).json(data)
+  })
+})
+
+app.use((req, res) => {
+  res.status(404).send('<h1>404</h1>')
 })
 
 app.listen(PORT, () => {
